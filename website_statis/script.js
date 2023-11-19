0e7406e2284c631db343ac5cb7235f2431c1df5a
Zakywhyn <139000513+Zakywhyn@users.noreply.github.com>
2023-11-19 18:38:44 +0700

    Add files via upload

---
+++ website_statis/script.js
@@ -0,0 +1,53 @@
+// alert('Selamat Datang di Pariwisata Indonesia')
+
+// navbar
+const menu = document.querySelector('.navbar #menu');
+const nav = document.querySelector('.navbar .navbar-nav');
+
+menu.addEventListener('click',function(){
+    nav.classList.toggle('active')
+});
+
+// galery
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 1500); // ganti foto setiap 1,5 seconds
+}
+
+function showTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+
+  document.getElementById("DisplayClock").innerText = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
